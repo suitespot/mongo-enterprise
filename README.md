4c024a2fdc5fe22b9e390fa5e4b4a0ad08cf5a91
Sorin Michnea <sorin.michnea@suitespottechnology.com>
2019-11-27 17:14:45 -0500

    Initial commit. Mongo-4.2.1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mongo-enterprise
-Repository to allow building a mongo-enterprise containe
+Repository to allow building a mongo-enterprise container as per the official instructions: https://docs.mongodb.com/manual/tutorial/install-mongodb-enterprise-with-docker/
+
+The Dockerfile is slightly modified to allow unattended build of the container.
---
+++ update-repo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MONGODB_VERSION=4.2
+DOCKER_USERNAME=suitespot
+
+curl -O --remote-name-all https://raw.githubusercontent.com/docker-library/mongo/master/$MONGODB_VERSION/{Dockerfile,docker-entrypoint.sh}
+chmod 755 ./docker-entrypoint.sh
+docker build \
+  --build-arg MONGO_PACKAGE=mongodb-enterprise \
+  --build-arg MONGO_REPO=repo.mongodb.com \
+  -t $DOCKER_USERNAME/mongo-enterprise:$MONGODB_VERSION .
