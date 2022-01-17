184c8b49c93fafd970ce93773c93a57c9581d0a0
Mateo Cuervo <mateo.cuervo@suitespottechnology.com>
2022-01-17 17:04:46 -0500

    si-7095 arm64

--- update-repo.sh
+++ update-repo.sh
@@ -5,7 +5,7 @@ DOCKER_USERNAME=suitespot
 
 curl -O --remote-name-all https://raw.githubusercontent.com/docker-library/mongo/master/$MONGODB_VERSION/{Dockerfile,docker-entrypoint.sh}
 chmod 755 ./docker-entrypoint.sh
-docker build \
+docker buildx build --platform linux/arm64 \
   --build-arg MONGO_PACKAGE=mongodb-enterprise \
   --build-arg MONGO_REPO=repo.mongodb.com \
   -t $DOCKER_USERNAME/mongo-enterprise:$MONGODB_VERSION .
