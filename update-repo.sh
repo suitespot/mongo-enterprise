1a8f3903708f231e429ffdec38b7c966e382a28f
Volodymyr Kudelin <volodymyr.kudelin@suitespottechnology.com>
2023-02-10 20:07:59 -0500

    Update update-repo.sh script

--- update-repo.sh
+++ update-repo.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# USAGE: 
+# Change MONGO_MAJOR and MONGO_VERSION to desired a version.
+# To build locally and push to repository, run:
+#   ./update-repo.sh --push
+
 MONGO_MAJOR=6.0
 MONGO_VERSION=6.0.4
 
@@ -10,4 +15,6 @@ chmod 755 ./docker-entrypoint.sh
 docker buildx build --platform linux/arm64,linux/amd64 \
   --build-arg MONGO_PACKAGE=mongodb-enterprise \
   --build-arg MONGO_REPO=repo.mongodb.com \
-  -t $DOCKER_USERNAME/mongo-enterprise:$MONGODB_VERSION .
+  -t $DOCKER_USERNAME/mongo-enterprise:$MONGO_VERSION \
+  -t $DOCKER_USERNAME/mongo-enterprise:latest \
+  . $1
